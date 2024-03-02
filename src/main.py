4e752bdad0281859c66713d3f876c09f88ffcfec
fastsnowy <61731151+fastsnowy@users.noreply.github.com>
2024-03-02 21:28:09 +0900

    ⚡️ CORSの設定

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["localhost:4200"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
